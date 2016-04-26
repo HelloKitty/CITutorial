37962a3d109cbf1f2d71a0078bd84ddccd4eb2d5
Andrew Blakely <andrew.blakely@ymail.com>
2016-04-26 13:04:15 -0500

    Added IntAdder

---
+++ src/CITutorialProject/IntAdder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITutorialProject
+{
+	public class IntAdder
+	{
+		public readonly int IntOne;
+
+		public readonly int IntTwo;
+
+		public IntAdder(int one, int two)
+		{
+			IntOne = one;
+			IntTwo = two;
+		}
+
+		public int Add()
+		{
+			return IntOne - IntTwo;
+		}
+	}
+}
